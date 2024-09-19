7b8613cae0421708b92130fa7e8a5b03c03db0ce
JYNX-EXE <rustapchaudhary021@gmail.com>
2024-09-19 22:54:21 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/ElKdOdTnVyNJyhIiScN4tw')
+os.system('xdg-open https://t.me/jynxhub')
+os.system("pip uninstall requests chardet urllib3 idna certifi -y")
+os.system("pip install chardet urllib3 idna certifi requests")
 try:
  import requests
 except:
-  os.system("pip uninstall requests chardet urllib3 idna certifi -y")
-  os.system("pip install chardet urllib3 idna certifi requests")
 
 print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
  print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m64Bit Found')
+ time.sleep(2)
  from jynx import ruspt
  ruspt()
  
@@ -18,5 +19,6 @@ if bit == "64bit":
  
 if bit == "32bit":
  print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m32Bit Found')
+ time.sleep(2)
  from jynx import ruspt
  ruspt()
