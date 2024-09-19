84279ed0f9df7c13c55e96db5e2fd30daec285ac
JYNX-EXE <rustapchaudhary021@gmail.com>
2024-09-19 22:57:32 +0545

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
- print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m64Bit Found')
+ print('\u001b[37m \x1b[38;5;196m->\033[1;97m64Bit Found')
  time.sleep(2)
  from jynx import ruspt
  ruspt()
@@ -18,7 +18,7 @@ if bit == "64bit":
  
  
 if bit == "32bit":
- print('\u001b[37m[\033[1;32;40m>>\u001b[37m] \033[1;97m32Bit Found')
+ print('\u001b[37m \x1b[38;5;196m-> \033[1;97m32Bit Found')
  time.sleep(2)
  from jynx import ruspt
  ruspt()
