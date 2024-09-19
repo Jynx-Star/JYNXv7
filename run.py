8aa7d292279da85047fcab266744f0fa18a73d58
JYNX-EXE <rustapchaudhary021@gmail.com>
2024-09-19 23:00:25 +0545

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,8 @@ import os, platform, time, sys
 os.system('xdg-open https://t.me/jynxhub')
 try:
  import requests
-except:
-	os.system("pip uninstall requests chardet urllib3 idna certifi -y")
-        os.system("pip install chardet urllib3 idna certifi requests")
+except:os.system("pip uninstall requests chardet urllib3 idna certifi -y")
+	os.system("pip install chardet urllib3 idna certifi requests")
 
 print('\u001b[37m \x1b[38;5;196m->\033[1;97mChecking For Update...')
 os.system('git pull --quiet ')
